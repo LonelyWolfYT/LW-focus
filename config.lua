77994dc8d1b0e8aa570d23169ed0252ed6671981
Lonely Wolf <119653343+LonelyWolfYT@users.noreply.github.com>
2025-10-17 21:28:05 +0530

    Code Optimized

--- config.lua
+++ config.lua
@@ -10,13 +10,3 @@ Config.zoomSpeed = 2.0
 Config.disableShooting = true
 Config.disableAiming = true
 Config.DisableInFirstPerson = true
-Config.CameraSwitch = true
-
-
--- Key used for switching camera (default: V)
--- Reference: https://docs.fivem.net/docs/game-references/controls/
-Config.CameraSwitchKey = 0 -- INPUT_SELECT_NEXT_CAMERA
-
--- Allowed camera modes (cycle will only toggle between these)
--- Available: FIRST_PERSON = 4, THIRD_PERSON_NEAR = 0, THIRD_PERSON_MEDIUM = 1, THIRD_PERSON_FAR = 2, CINEMATIC = 3
-Config.AllowedCameraModes = { FIRST_PERSON = true, THIRD_PERSON_MEDIUM = true }  --(under development)
